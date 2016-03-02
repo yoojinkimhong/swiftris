7cd2582da7487b277ea002bb182dbb0e8acc14ff
YooJ <yoojinkimhong@gmail.com>
2016-03-01 19:02:24 -0800

    Finished Shaping Up

---
+++ com.yoojinkimhong.swiftris/LineShape.swift
@@ -0,0 +1,28 @@
+//
+//  LineShape.swift
+//  com.yoojinkimhong.swiftris
+//
+//  Created by YooJin Hong on 3/1/16.
+//  Copyright © 2016 YooJin Hong. All rights reserved.
+//
+
+class LineShape: Shape {
+    
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.Ninety:     [(-1,0), (0, 0), (1, 0), (2, 0)],
+            Orientation.OneEighty:  [(0, 0), (0, 1), (0, 2), (0, 3)],
+            Orientation.TwoSeventy: [(-1,0), (0, 0), (1, 0), (2, 0)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[FourthBlockIdx]],
+            Orientation.Ninety:     blocks,
+            Orientation.OneEighty:  [blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: blocks
+        ]
+    }
+}
---
+++ shape.swift
@@ -0,0 +1,121 @@
+//
+//  shape.swift
+//  com.yoojinkimhong.swiftris
+//
+//  Created by YooJin Hong on 2/28/16.
+//  Copyright © 2016 YooJin Hong. All rights reserved.
+//
+
+import SpriteKit
+
+let NumOrientations: UInt32 = 4
+
+enum Orientation: Int, CustomStringConvertible {
+    case Zero = 0, Ninety, OneEighty, TwoSeventy
+    
+    var description: String {
+        switch self {
+        case .Zero:
+            return "0"
+        case .Ninety:
+            return "90"
+        case .OneEighty:
+            return "180"
+        case .TwoSeventy:
+            return "270"
+        }
+    }
+    
+    static func random() -> Orientation {
+        return Orientation(rawValue:Int(arc4random_uniform(NumOrientations)))!
+    }
+    
+//#1
+    static func rotate(orientation:Orientation, clockwise: Bool) -> Orientation {
+        var rotated = orientation.rawValue + (clockwise ? 1 : -1)
+        if rotated > Orientation.TwoSeventy.rawValue {
+            rotated = Orientation.Zero.rawValue
+        } else if rotated < 0 {
+            rotated = Orientation.TwoSeventy.rawValue
+        }
+        return Orientation(rawValue:rotated)!
+    }
+}
+// The number of total shape varieties
+let NumShapeTypes: UInt32 = 7
+
+//Shape indexes
+let FirstBlockIdx: Int = 0
+let SecondBlockIdx: Int = 1
+let ThirdBlockIdx: Int = 2
+let FourthBlockIdx: Int = 3
+
+class Shape: Hashable, CustomStringConvertible {
+    // The color of the shape
+    let color:BlockColor
+
+    //The Blocks comprising the shape
+    var blocks = Array<Block>()
+    // The current orientation of the shape
+    var orientation: Orientation
+    //The column and row representing the shape's and anchor points
+    var column, row:Int
+
+    // Reuired Overrides
+//#2
+    // Subclass must override this property
+    var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [:]
+    }
+// #3
+    // Subclass must override this property
+    var bottomBlocksForOrientations:[Orientation: Array<Block>] {
+        return [:]
+    }
+//#5
+    var bottomBlocks:Array<Block> {
+        guard let bottomBlocks = bottomBlocksForOrientations[orientation] else {
+            return []
+        }
+        return bottomBlocks
+    }
+    
+    // Hashable
+    var hashValue:Int {
+//#6
+        return blocks.reduce(0) { $0.hashValue ^ $1.hashValue }
+    }
+    
+    //CustomStringConvertible
+    var description:String {
+        return "\(color) block facing \(orientation): \(blocks[FirstBlockIdx]), \(blocks[SecondBlockIdx]), \(blocks[ThirdBlockIdx]), \(blocks[FourthBlockIdx])"
+    }
+    
+    init(column:Int, row:Int, color: BlockColor, orientation:Orientation) {
+        self.color = color
+        self.column = column
+        self.row = row
+        self.orientation = orientation
+        initializeBlocks()
+    }
+    
+    // #6
+    convenience init(column:Int, row:Int) {
+        self.init(column:column, row:row, color:BlockColor.random(), orientation:Orientation.random())
+    }
+    
+    final func initializeBlocks() {
+        guard let blockRowColumnTranslations = blockRowColumnPositions[orientation] else {
+            return
+        }
+        // #8
+        blocks = blockRowColumnTranslations.map { (diff) -> Block in
+            return Block(column: column + diff.columnDiff, row: row + diff.rowDiff, color: color)
+        }
+    }
+}
+
+func ==(lhs: Shape, rhs: Shape) -> Bool {
+    return lhs.row == rhs.row && lhs.column == rhs.column
+}
+
